e0dfedd35b82f5fe7da9926cdf66f1ddde55626f
Masoom-Wahid <masoom3892@gmail.com>
2024-05-18 19:55:56 +0430

    Fix : Fixed the bug in get_abs_path

--- src/tests/get_abs_path.rs
+++ src/tests/get_abs_path.rs
@@ -5,7 +5,7 @@ pub fn get_abs_path(name : &str)  -> Result<String>{
    /*
    Used for getting the absloute path to anything in the test_dir
     */ 
-    let abs_path : String= canonicalize(PathBuf::from(format!("./src/lib/tests/data/{}",name)))?
+    let abs_path : String= canonicalize(PathBuf::from(format!("./src/tests/data/{}",name)))?
                             .to_str()
                             .unwrap()
                             .try_into()?;
